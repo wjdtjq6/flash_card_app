fc345f4855e8bf5112db322718aa11171d34dc3d
jimmy <wjdtjq6@gmail.com>
2024-04-17 10:34:24 +0900

    Initial Commit

---
+++ flah_card_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flah_card_app
+//
+//  Created by t2023-m0032 on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flah_card_app/flah_card_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  flah_card_appApp.swift
+//  flah_card_app
+//
+//  Created by t2023-m0032 on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct flah_card_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
